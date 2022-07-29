e7aff115acd975462c163d7b4d22c86c29c85837
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-07-29 17:16:31 +0900

    feature: Unit CRUD

--- goco/src/main/java/com/pchr/repository/UnitRepository.java
+++ goco/src/main/java/com/pchr/repository/UnitRepository.java
@@ -1,5 +1,7 @@
 package com.pchr.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.pchr.entity.Unit;
 @Repository
 public interface UnitRepository extends JpaRepository<Unit, Long>{
 
+	public List<Unit> findByparentUnit(Unit unitId);
+	
 }
--- goco/src/main/java/com/pchr/service/UnitService.java
+++ goco/src/main/java/com/pchr/service/UnitService.java
@@ -1,5 +1,14 @@
 package com.pchr.service;
 
-public interface UnitService {
+import java.util.List;
 
+import com.pchr.dto.UnitDTO;
+
+public interface UnitService {
+	
+	public List<UnitDTO> unitAll();
+	public void unitInsert(UnitDTO unitDTO);
+	public void unitUpdate(UnitDTO unitDTO);
+	public List<UnitDTO> deptUnit(UnitDTO unit);
+	public void unitDelete(UnitDTO unitDTO);
 }
