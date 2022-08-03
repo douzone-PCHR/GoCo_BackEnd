ab6efc9da8eb8a2d3d9bfb85631fbabe5cb2d51c
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-08-03 19:31:59 +0900

    기능 수정: auth 권한 All / Auditing / Add Email Pom.xml

--- goco/src/main/java/com/pchr/goco/GocoApplication.java
+++ goco/src/main/java/com/pchr/goco/GocoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.pchr.api" , "com.pchr.service" , "com.pchr.service.impl","com.pchr.config" ,"com.pchr.jwt"})
 @EntityScan(basePackages = {"com.pchr.entity"})
 @EnableJpaRepositories(basePackages = {"com.pchr.repository"})
+@EnableJpaAuditing
 public class GocoApplication {
 
 	public static void main(String[] args) {
