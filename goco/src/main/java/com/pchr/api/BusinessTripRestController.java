4499e5ab19fb5796d717ca013ce499233febc062
qqqwwwddd <qqqwwwddd1@gmail.com>
2022-08-04 11:45:10 +0900

    FEAT: done BusinessTrip API

--- goco/src/main/java/com/pchr/api/BusinessTripRestController.java
+++ goco/src/main/java/com/pchr/api/BusinessTripRestController.java
@@ -1,8 +1,70 @@
 package com.pchr.api;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pchr.dto.BusinessTripDTO;
+import com.pchr.service.impl.BusinessTripServiceImpl;
 
 @RestController
 public class BusinessTripRestController {
+	@Autowired
+	BusinessTripServiceImpl businessTripService;
+
+	// 출장 신청 리스트 (사원)
+	@GetMapping(value = "/business/{empNum}")
+	public List<BusinessTripDTO> findBusinessTripByEmployeeEmpNum(@PathVariable Long empNum) {
+		List<BusinessTripDTO> businessTripList = businessTripService.getAllBusinessTrip(empNum);
+		System.out.println(businessTripList);
+		return businessTripList;
+	}
+
+	// 출장 신청 리스트 (팀장)
+	@GetMapping(value = "/business/approve/{unitId}")
+	public List<BusinessTripDTO> findBusinessTripByEmployeeUnitId(@PathVariable Long unitId) {
+		List<BusinessTripDTO> businessTripList = businessTripService.getAllTeamBusinessTrip(unitId);
+		System.out.println(businessTripList);
+		return businessTripList;
+	}
+
+	// 출장 신청
+	@PostMapping(value = "/business", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void insertBusinessTrip(@RequestBody BusinessTripDTO businessTripDTO) {
+		System.out.println(businessTripDTO);
+		businessTripService.insertBusinessTrip(businessTripDTO);
+	}
+
+	// 출장 수정 (사원) , 출장 취소 approveYn.APPROVE_CANCEL(사원)
+	@Transactional
+	@PutMapping(value = "/business/{businessTripId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void updateBusiness(@RequestBody BusinessTripDTO businessTripDTO, @PathVariable Long businessTripId) {
+		System.out.println(businessTripDTO);
+		businessTripDTO.setBusinessTripId(businessTripId);
+		businessTripService.updateBusiness(businessTripDTO);
+	}
+
+	// 출장 결재 (팀장)
+	@Transactional
+	@PutMapping(value = "/business/approve/{businessTripId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void approveBusiness(@RequestBody BusinessTripDTO businessTripDTO) {
+		businessTripService.approveBusiness(businessTripDTO);
+	}
+
+	// check date
+	@GetMapping(value = "/business/check")
+	public List<BusinessTripDTO> checkBusiness(@RequestBody BusinessTripDTO businessTripDTO) {
+
+		return businessTripService.checkBusiness(businessTripDTO);
+	}
 
 }
--- goco/src/main/java/com/pchr/repository/BusinessTripRepository.java
+++ goco/src/main/java/com/pchr/repository/BusinessTripRepository.java
@@ -1,5 +1,26 @@
 package com.pchr.repository;
 
-public interface BusinessTripRepository {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.pchr.entity.BusinessTrip;
+
+@Repository
+public interface BusinessTripRepository extends JpaRepository<BusinessTrip, Long> {
+
+	// 출장 신청 리스트 (사원)
+	List<BusinessTrip> findAllByEmployeeEmpNum(Long empNum);
+
+	// 출장 신청 리스트 (팀장)
+	List<BusinessTrip> findAllByEmployeeUnitUnitId(Long unitId);
+
+	// checkBusiness
+	@Query(value = "select * from business_trip where emp_num = :empNum and business_trip_start_date <= :endDate and business_trip_end_date >= :startDate", nativeQuery = true)
+	public List<BusinessTrip> checkBusiness(@Param("empNum") Long empNum, @Param("startDate") Date startDate,
+			@Param("endDate") Date endDate);
 }
