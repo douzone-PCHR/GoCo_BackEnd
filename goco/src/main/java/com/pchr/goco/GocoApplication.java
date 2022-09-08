256b892d8d20d01642f4f0653ad9f838fb152454
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-09-08 19:44:23 +0900

    feat : DB TimeZone Setting

--- goco/src/main/java/com/pchr/goco/GocoApplication.java
+++ goco/src/main/java/com/pchr/goco/GocoApplication.java
@@ -1,5 +1,9 @@
 package com.pchr.goco;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -20,5 +24,10 @@ public class GocoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GocoApplication.class, args);
 	}
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+    }
 
 }
